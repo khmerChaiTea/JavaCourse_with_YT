38d94724e222ed22cfa60635019f1fad1d9f143a
pchai <prempreychhay@gmail.com>
2024-08-30 10:41:03 -0700

    Type Conversion in Java

--- JavaCourse/Hello.java
+++ JavaCourse/Hello.java
@@ -1,12 +1,26 @@
 
 class Hello // A file name
 {
-    public static void main(String a[])
+    public static void main(String args[])
     {
-        // Literal in Java
-        char c = 'a';
-        c++;
-        System.out.println(c);
+        // Type Conversion in Java - casting
+        // byte b = 127;
+        int a = 12;
+        byte k = (byte)a;
+
+        float f = 5.6f;
+        int t = (int)f;
+
+        System.out.println(k);
+        System.out.println(a);
+        System.out.println(t);  
+
+        byte c = 10;
+        byte d = 30;
+
+        int result = c * d; // Type promotion
+
+        System.out.println(result);  
     }
 }
 
