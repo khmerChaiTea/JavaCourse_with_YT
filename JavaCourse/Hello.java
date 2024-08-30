273bc14a21a106ded9a84f26feaebf7b8b5cfe43
pchai <prempreychhay@gmail.com>
2024-08-29 19:44:50 -0700

    My Java Project

---
+++ JavaCourse/Hello.java
@@ -0,0 +1,15 @@
+
+class Hello // A file name
+{
+    // Java is object oriented
+    public static void main(String a[])
+    {
+        System.out.println("Hello World");
+    }
+}
+
+// To compile in terminal: javac Hello.java
+// To output to console: java Hello
+// JVM - Java Virtual Machine
+// JRE - Java Runtime Environment
+// WORA - Write once and run anywhere
