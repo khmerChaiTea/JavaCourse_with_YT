3a50b561de3a6d97c2ae4407c0218981d35921a5
pchai <prempreychhay@gmail.com>
2024-08-29 20:20:19 -0700

    Variables in Java

--- JavaCourse/Hello.java
+++ JavaCourse/Hello.java
@@ -4,9 +4,15 @@ class Hello // A file name
     // Java is object oriented
     public static void main(String a[])
     {
-        System.out.println("Hello World");
+        // Variables in Java
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);
+        // System.out.println(8 + 7);        
     }
 }
+
 
 // To compile in terminal: javac Hello.java
 // To output to console: java Hello
