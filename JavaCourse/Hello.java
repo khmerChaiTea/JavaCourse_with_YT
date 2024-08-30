723d4ce9686de402277ad64d197a600e385a0f47
pchai <prempreychhay@gmail.com>
2024-08-30 10:27:07 -0700

    Literal in Java

--- JavaCourse/Hello.java
+++ JavaCourse/Hello.java
@@ -3,18 +3,10 @@ class Hello // A file name
 {
     public static void main(String a[])
     {
-        // Data types in Java
-        int num1 = 9;
-        byte by = 127;
-        short sh = 558;
-        long l = 419664l;
-
-        float f = 5.8f;
-        double d = 5.8;
-
-        char c = 'k'; // Only single quote
-
-        boolean b = true;
+        // Literal in Java
+        char c = 'a';
+        c++;
+        System.out.println(c);
     }
 }
 
