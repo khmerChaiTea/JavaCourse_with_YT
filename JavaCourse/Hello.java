e2cd03a7510f40ccfcabe00a71df3bdb5a18fea9
pchai <prempreychhay@gmail.com>
2024-09-01 08:18:39 -0700

    Relational Operators in Java

--- JavaCourse/Hello.java
+++ JavaCourse/Hello.java
@@ -3,20 +3,14 @@ class Hello
 {
     public static void main(String args[])
     {
-        // Arithmetic Operators in Java
-        int num1 = 7;
-        // int num2 = 5;
+        // Relational Operators in Java
+        double x = 8.8;
+        double y = 9.8;
 
-        // int result = num1 % num2;
+        // int a = 8;
+        // int b = 6;
 
-        // num1 = num1 + 2;
-        // num1 *= 8; // Short cut
-        // num1++; // Post - Increment
-        // ++num1; // Pre - Increment (when try to fetch the values)
-        // num1--; // Decrement
-
-        int result = num1++;  // Fetch the value and then increment value
-        // int result = ++num1;  // increment value and then fetch the value
+        boolean result = x >= y;
 
         System.out.println(result);
     }
