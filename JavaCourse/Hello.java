52f04239224fa8d85bb719a492c694edac7359ae
pchai <prempreychhay@gmail.com>
2024-09-13 11:02:32 -0700

    Ternary Operator in Java

--- JavaCourse/Hello.java
+++ JavaCourse/Hello.java
@@ -3,18 +3,19 @@ class Hello
 {
     public static void main(String args[])
     {
-        // If Else If in Java
-        int x = 8;
-        int y = 7;
-        int z = 9;
+        // Ternary Operator in Java
+        int n = 4;
+        int result = 0;
 
+        // if (n % 2 == 0)
+        //     result = 10;
+        // else
+        //     result = 20;
 
-        if (x > y && x > z) // False
-            System.out.println(x);
-        else if (y > z)
-            System.out.println(y);
-        else
-            System.out.println(z);
+        // Ternary operator --> ? T:F
+        result = n % 2 == 0 ? 10 : 20;
+
+        System.out.println(result);
     }
 }
 
